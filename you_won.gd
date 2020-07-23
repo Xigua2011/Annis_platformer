28c03aa868a1ff56498db3ca9e242e8010f25c1b
electronstudio <misc@fantastic.me.uk>
2020-07-23 21:07:05 +0100

    various fixes

--- you_won.gd
+++ you_won.gd
@@ -11,7 +11,7 @@ var time = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta
-	if Input.is_action_pressed('jump'):
+	if Input.is_action_just_pressed('jump'):
 		get_tree().change_scene("res://Title .tscn")
 	if time > 4:
 		get_tree().change_scene("res://Title .tscn")
